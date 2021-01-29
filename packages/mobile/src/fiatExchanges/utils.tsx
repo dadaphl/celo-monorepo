cfb9a16df4dd5c6c7089d4e8c395a9abf244ad27
Pedro Gutiérrez <pedrogp9693@gmail.com>
2021-01-29 16:40:09 +0100

    WIP

--- packages/mobile/src/fiatExchanges/utils.tsx
+++ packages/mobile/src/fiatExchanges/utils.tsx
@@ -1,3 +1,5 @@
+import { RampInstantSDK } from '@ramp-network/ramp-instant-sdk'
+
 import { SIMPLEX_URI } from 'src/config'
 import { CURRENCY_ENUM } from 'src/geth/consts'
 import { LocalCurrencyCode } from 'src/localCurrency/consts'
@@ -15,4 +17,16 @@ export const openMoonpay = (currencyCode: LocalCurrencyCode, currencyToBuy: CURR
 
 export const openSimplex = (account: string | null) => {
   navigateToURI(`${SIMPLEX_URI}?address=${account}`)
+}
+
+export const openRamp = (account: string | null) => {
+  new RampInstantSDK({
+    hostAppName: 'your dApp',
+    hostLogoUrl: 'https://yourdapp.com/yourlogo.png',
+    swapAmount: '150000000000000000000', // 150 ETH in wei
+    swapAsset: 'ETH',
+    userAddress: 'user blockchain address',
+  })
+    .on('*', (event: any) => console.log(event))
+    .show()
 }
